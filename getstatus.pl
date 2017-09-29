f77403245a992206d56a3c1bbc0ff976f870c550
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2017-09-29 10:45:23 -0300

    Get data from Zabbix.

--- getstatus.pl
+++ getstatus.pl
@@ -2,12 +2,35 @@
 use strict;
 use warnings;
 use Config::Simple;
+use HTTP::Request;
+use LWP::UserAgent;
 
 my %CONFIG;
 
 Config::Simple->import_from($ARGV[0], \%CONFIG);
 
-while (each %CONFIG) {
-    print "Value for $_  is $CONFIG{$_}\n";	
+my $req = HTTP::Request->new('POST', $CONFIG{host});
+$req->header('Content-Type' => 'application/json');
+$req->content(<<EOF
+{
+   "jsonrpc": "2.0",
+   "method": "host.get",
+   "params": {
+        "output": "extend"
+    },
+    "id": 2,
+    "auth": "$CONFIG{auth}"
 }
+EOF
+);
 
+my $lwp = LWP::UserAgent->new;
+my $res = $lwp->request($req);
+
+
+if ($res->is_success) {
+    print $res->decoded_content;
+}
+else {
+    print STDERR $res->status_line, "\n";
+}
